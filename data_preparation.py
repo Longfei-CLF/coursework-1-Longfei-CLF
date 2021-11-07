2211ad2c6dd8a998006ad5ced2f8fd13e50ab21b
Longfei Chen <Longfei.chen.18@ucl.ac.uk>
2021-11-07 18:36:42 +0000

    Read data file

--- data_preparation.py
+++ data_preparation.py
@@ -1,14 +1,10 @@
-
-#import pandas as pd
-orgianl_file = "GVP_Eruption_Results.xls"
-#df = pd.read_excel(orgianl_file, sheet_name='Eruption List', engine='xlrd')
-# orgianl_file = "GVP.csv"
-# df = pd.read_csv(orgianl_file, sep="", engine='python')
-
+# Start to use pandas
 import pandas as pd
-from bs4 import BeautifulSoup
-import os
-
+orgianl_file = "GVP_Eruption_Results.xls"
+## Outstanding issue in reading the file
+# Orginal file downloaded from the website cannot be read by using df = pd.read_excel(orgianl_file, sheet_name='Eruption List')
+# Error message: xlrd.biffh.XLRDError: Unsupported format, or corrupt file: Expected BOF record; found b'<?xml ve'
+# I did some research and found the orginal file might be in xml format. Then I tried use beautifulsoup to convert the file into xlsx.
 def convert_to_xlsx():
     with open(orgianl_file) as xml_file:
         soup = BeautifulSoup(xml_file.read(), 'xml')
@@ -22,11 +18,13 @@ def convert_to_xlsx():
             pd.DataFrame(sheet_as_list).to_excel(writer, sheet_name=sheet.attrs['ss:Name'], index=False, header=False)
 
         writer.save()
+# However, the converted xlsx file is incomplete.
+# By following Sarah's suggestion, I opened the orginal file in excel and saved it as a new xlsx file called "New_GVP_Eruption_Results.xlsx"
 
+## Beign the data prepartion with new orginal file.
+new_orgianl_file = "New_GVP_Eruption_Results.xls"
 
-
-convert_to_xlsx()
-df=pd.read_excel('Clean.xlsx', sheet_name='Eruption List', skiprows=1)
+df=pd.read_excel(new_orgianl_file, sheet_name='Eruption List', skiprows=1)
 
 print(df.columns)
 print(df.shape)
--- test.py
+++ test.py
@@ -1,1 +1,8 @@
-print ('Hello')
+
+import pandas as pd
+orgianl_file = "GVP_Eruption_Results.xls"
+df = pd.read_excel(orgianl_file, sheet_name='Eruption List', engine='xlrd')
+# orgianl_file = "GVP.csv"
+# df = pd.read_csv(orgianl_file, sep="", engine='python')
+
+
