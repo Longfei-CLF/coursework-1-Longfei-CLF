1b7914b31fafad22fdeb55f60d3bcdbce8d16a65
Longfei Chen <Longfei.chen.18@ucl.ac.uk>
2021-11-09 21:46:00 +0000

    exploration finished

--- data_exploration.py
+++ data_exploration.py
@@ -1,16 +1,122 @@
-# Write code that explores your data set
 # Start to use pandas
 import numpy as np
+from numpy.lib.function_base import append
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# Load the cleaned data set
-cleaned_file = "Cleaned_GVP_Eruption_Results.xlsx"
-df = pd.read_excel(cleaned_file)
+def xaxis_generation():
+    """
+    Generate the common used xaxis for the following three diagrams
+    return: A list of year from 1700 to 2021
+    rtype: List
+    """
+    xaxis = []
+    for i in range (1700, 2021):
+        xaxis.append(i)
+    return xaxis
 
-# Check the dtype of the prepared data
-print(df.dtypes)
+def Number_Eruptions(df, xaxis):
+    """
+    Show the trend of number of eruptions verus time
+    param: df Dataframe the prepared data, xaxis used for plotting
+    return: Line plot of number of eruptions over time
+    rtype: Graph
+    """
+    NumErup = []
+    for i in xaxis:
+        count = 0
+        for x in df['Start Year']:
+            if i == x:
+                count = count + 1
+        NumErup.append(count)
 
+    plt.cla()
+    plt.plot(xaxis, NumErup)
+    plt.xlabel("Eruption Start Year")
+    plt.ylabel("Number of Eruptions")
+    plt.title("The relationship between Year and Number of Eruptions")
+    plt.savefig('images/Number of Eruptions over Year.png')
+    
+def Eruption_Duration(df, xaxis):
+    """
+    Show the trend of eruptions duration verus time
+    param: df Dataframe the prepared data, xaxis used for plotting
+    return: Line plot of eruptions duration over time
+    rtype: Graph
+    """
+    ErupDur_with_Outliner = []
+    ErupDur_without_Outliner = []
+    for i in xaxis:
+        dur_with_Outliner = []
+        dur_without_Outliner = []
+        row = 0
+        for x in df['Start Year']:
+            if i == x:
+                day = df.loc[row,'Eruption Duration (d)']
+                if day < 10000:
+                    dur_with_Outliner.append(day)
+                    dur_without_Outliner.append(day)
+                else:
+                    dur_with_Outliner.append(day)
+            row = row + 1
+        if sum(dur_with_Outliner) == 0: ErupDur_with_Outliner.append(0)
+        else: ErupDur_with_Outliner.append(np.mean(dur_with_Outliner))
+        if sum(dur_without_Outliner) == 0: ErupDur_without_Outliner.append(0)
+        else: ErupDur_without_Outliner.append(np.mean(dur_without_Outliner))        
 
+    plt.cla()
+    plt.plot(xaxis, ErupDur_with_Outliner)
+    plt.xlabel("Eruption Start Year")
+    plt.ylabel("Eruption Durations (d)")
+    plt.title("The relationship between Year and Eruption Durations (with outliners)")
+    plt.savefig('images/Eruptions Durations over Year with outliners.png')
 
+    plt.cla()
+    plt.plot(xaxis, ErupDur_without_Outliner)
+    plt.xlabel("Eruption Start Year")
+    plt.ylabel("Eruption Durations (d)")
+    plt.title("The relationship between Year and Eruption Durations (without outliners)")
+    plt.savefig('images/Eruptions Durations over Year without outliners.png')
 
+def VEI(df, xaxis):
+    """
+    Show the trend of volcano eruption index (VEI) verus time
+    param: df Dataframe the prepared data, xaxis used for plotting
+    return: Line plot of VEI over time
+    rtype: Graph
+    """
+    VEI = []
+    for i in xaxis:
+        index = []
+        row = 0
+
+        for x in df['Start Year']:
+            if i == x:
+                index.append(df.loc[row,'Volcano Eruption Index (VEI)'])
+            row = row + 1
+        if sum(index) == 0: VEI.append(0)
+        else: VEI.append(np.mean(index))
+
+    plt.cla()
+    plt.plot(xaxis, VEI)
+    plt.xlabel("Eruption Start Year")
+    plt.ylabel("Volcano Eruption Index (VEI)")
+    plt.title("The relationship between Year and VEI")
+    plt.savefig('images/VEI over Year.png')
+
+if __name__ == '__main__':
+    cleaned_file = "Cleaned_GVP_Eruption_Results.xlsx"
+    df = pd.read_excel(cleaned_file)
+    df.rename(columns={'Sta_yr':'Start Year', 'Erup_dur': 'Eruption Duration (d)',
+        'VEI': 'Volcano Eruption Index (VEI)'}, inplace=True)
+    #Basic stat
+    print(df.describe())
+    # Outliners
+    df.plot.box(subplots=True)
+    plt.show()
+    print(df.loc[df['Eruption Duration (d)'] > 10000]) # No need to drop, reasonable outliners
+    # Plot graph to anwer the three questions
+    Year = xaxis_generation()
+    Number_Eruptions(df, Year)
+    Eruption_Duration(df, Year)
+    VEI(df, Year)
--- data_preparation.py
+++ data_preparation.py
@@ -3,12 +3,12 @@ import numpy as np
 import pandas as pd
 from bs4 import BeautifulSoup
 
-orgianl_file = "GVP_Eruption_Results.xls"
 ## Outstanding issue in reading the file
 # Orginal file downloaded from the website cannot be read by using df = pd.read_excel(orgianl_file, sheet_name='Eruption List')
 # Error message: xlrd.biffh.XLRDError: Unsupported format, or corrupt file: Expected BOF record; found b'<?xml ve'
 # I did some research and found the orginal file might be in xml format. Then I tried use beautifulsoup to convert the file into xlsx.
 def convert_to_xlsx():
+    orgianl_file = "GVP_Eruption_Results.xls"
     with open(orgianl_file) as xml_file:
         soup = BeautifulSoup(xml_file.read(), 'xml')
         soup.prettify(formatter='html')
@@ -25,48 +25,49 @@ def convert_to_xlsx():
 # By following Sarah's suggestion, I opened the orginal file in excel and saved it as a new xlsx file called "New_GVP_Eruption_Results.xlsx"
 
 ## Beign the data prepartion with new orginal file.
-new_orgianl_file = "New_GVP_Eruption_Results.xlsx"
+def prepare_data(df):
+    """
+    Prepare the original data
+    param:
+        df Dataframe the raw data
+    return:
+        The prepared data
+    rtype:
+        Dataframe
+    """
 
-# Load the data set from the file and skip the first row because it has a header
-df = pd.read_excel(new_orgianl_file, sheet_name='Eruption List', skiprows=1)
-
-# Print the columns name
-print('Initial columns:')
-print(df.columns)
-
-# Rename final used columns
-df.rename(columns={'Volcano Number':'Vol_num', 'Volcano Name':'Vol_name',
-        'Start Year':'Sta_yr', 'Start Month':'Sta_mo', 'Start Day':'Sta_dy',
-        'End Year':'End_yr', 'End Month':'End_mo', 'End Day':'End_dy',},
+    print(df.columns)
+    df.rename(columns={'Volcano Name':'Vol_name', 'Start Year':'Sta_yr', 
+        'Start Month':'Sta_mo', 'Start Day':'Sta_dy', 'End Year':'End_yr', 
+        'End Month':'End_mo', 'End Day':'End_dy',},
         inplace=True)
-# Before dropping 'Eruption Category', obtain all confirmed eruptions
-df = df[df['Eruption Category'] == 'Confirmed Eruption']
-# Only obtain final used columns
-final_cols = ['Vol_num', 'Vol_name', 'Sta_yr', 'Sta_mo', 'Sta_dy', 
-    'End_yr', 'End_mo', 'End_dy', 'VEI']
-df = df[final_cols]
+    df = df[df['Eruption Category'] == 'Confirmed Eruption']
+    final_cols = ['Vol_name', 'Sta_yr', 'Sta_mo', 'Sta_dy', 
+        'End_yr', 'End_mo', 'End_dy', 'VEI']
+    df = df[final_cols]
 
-# Delete data earlier than 1700 due to limit of dtype (datetime64[ns]), 585 years (2^64 nanoseconds).
-df.drop(df[(df.Sta_yr <= 1700)].index, inplace=True)
-# Replace all 0 into NaN
-df.replace(0,np.nan,inplace=True)
-# Check for missing values
-print(df.isna().sum())
-# Remove the rows contain missing values
-df.dropna(axis=0, how='any', inplace=True)
+    # Delete data earlier than 1700 due to limit of dtype (datetime64[ns]), 585 years (2^64 nanoseconds).
+    df.drop(df[(df.Sta_yr <= 1700)].index, inplace=True)
 
-#Convert dtype from object to int
-df = df.convert_dtypes()
-# Create the start date column and convert the data type from str into Datetime64
-df['Sta_date'] = df.Sta_yr.astype(str) + ' ' + df.Sta_mo.astype(str) + ' ' + df.Sta_dy.astype(str)
-df['Sta_date'] = pd.to_datetime(df['Sta_date'],format='%Y-%m-%d')
-df['End_date'] = df.End_yr.astype(str) + ' ' + df.End_mo.astype(str) + ' ' + df.End_dy.astype(str)
-df['End_date'] = pd.to_datetime(df['End_date'])
-# Calculate the eruption duration
-df['Erup_dur'] = (df.End_date - df.Sta_date).dt.days
+    df.replace(0,np.nan,inplace=True)
+    print(df.isna().sum())
+    df.dropna(axis=0, how='any', inplace=True)
+    df = df.convert_dtypes()
+    df['Sta_date'] = df.Sta_yr.astype(str) + ' ' + df.Sta_mo.astype(str) + ' ' + df.Sta_dy.astype(str)
+    df['Sta_date'] = pd.to_datetime(df['Sta_date'],format='%Y-%m-%d')
+    df['End_date'] = df.End_yr.astype(str) + ' ' + df.End_mo.astype(str) + ' ' + df.End_dy.astype(str)
+    df['End_date'] = pd.to_datetime(df['End_date'])
+    df['Erup_dur'] = (df.End_date - df.Sta_date).dt.days
 
-# Check all columns and dtype are correct
-print(df.info(verbose=True))
+    df_prepared = pd.DataFrame(df, columns=['Vol_name', 'Sta_yr', 'Erup_dur', 'VEI'])
+    return df_prepared
 
-# Export the cleaned data as Excel file for exploration
-df.to_excel(r'Cleaned_GVP_Eruption_Results.xlsx', index=False)
+if __name__ == '__main__':
+    new_original_file = "New_GVP_Eruption_Results.xlsx"
+    df_original = pd.read_excel(new_original_file, sheet_name='Eruption List', skiprows=1)
+    df_prepared = prepare_data(df_original)
+
+    print(df_prepared.info(verbose=True))
+    print(df_prepared.head())
+
+    df_prepared.to_excel('Cleaned_GVP_Eruption_Results.xlsx', index=False)
