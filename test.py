4146686907ff209ba3060e59effa7867a8626713
Longfei Chen <Longfei.chen.18@ucl.ac.uk>
2021-11-10 07:56:09 +0000

    delete test file

--- test.py
+++
@@ -1,8 +0,0 @@
-
-import pandas as pd
-orgianl_file = "GVP_Eruption_Results.xls"
-df = pd.read_excel(orgianl_file, sheet_name='Eruption List', engine='xlrd')
-# orgianl_file = "GVP.csv"
-# df = pd.read_csv(orgianl_file, sep="", engine='python')
-
-
